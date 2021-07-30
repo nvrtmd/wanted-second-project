bc9d7f3875bebbc9b892cd62608f9a55a402960d
백진수 <bjinsu93@gmail.com>
2021-07-30 12:53:41 +0900

    feat : Add Item Component

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,9 +1,50 @@
 import React from 'react'
+import styled from 'styled-components'
 
-class Card extends React.Component {
+class Item extends React.Component {
   render() {
-    return <div>Card</div>
+    const { title, brand, price } = this.props.item
+    return (
+      <ItemWrapper>
+        <ItemTitle>{title}</ItemTitle>
+        <ItemDetail>
+          <span>{brand}</span>
+          <span>{price}</span>
+        </ItemDetail>
+      </ItemWrapper>
+    )
   }
 }
 
-export default Card
+export default Item
+
+const ItemWrapper = styled.li`
+  margin: 36px 0;
+  padding: 34px;
+  width: 630px;
+  border: 5px solid #2d3ff3;
+  border-radius: 15px;
+  background: #ffffff;
+`
+
+const ItemTitle = styled.h2`
+  color: #000000;
+  font-family: Roboto;
+  font-size: 29px;
+  font-weight: bold;
+`
+const ItemDetail = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  span:nth-child(1) {
+    color: #707070;
+    font-size: 24px;
+  }
+
+  span:nth-child(2) {
+    color: #000000;
+    font-size: 26px;
+  }
+`
