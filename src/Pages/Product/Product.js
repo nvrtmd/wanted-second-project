2f37fdaa2da539d9fa36348a72f997fca62afba2
hami-dev <rinrin.xml@gmail.com>
2021-07-31 10:18:13 +0900

    feat : about page 추가

--- src/Pages/Product/Product.js
+++ src/Pages/Product/Product.js
@@ -30,7 +30,7 @@ class Product extends React.Component {
   checkRandomNumber(num) {
     let data = []
     data = JSON.parse(localStorage.getItem('watched')) || []
-    console.log('checking ' + num)
+    // console.log('checking ' + num)
     for (let i = 0; i < data.length; i++) {
       if (data[i].index === num) {
         if (!data[i].interest) {
@@ -64,7 +64,7 @@ class Product extends React.Component {
 
   //[관심없어요] 버튼 누를 시 동작
   addDislikeProduct(product, index) {
-    console.log(`add ${index} dislike`)
+    // console.log(`add ${index} dislike`)
     alert('해당 상품을 앞으로 다시 보지 않습니다.')
     // 랜덤 상품으로 이동
     let interestResult = this.setInterest(index)
@@ -138,7 +138,7 @@ class Product extends React.Component {
   render() {
     const { params } = this.props.match
     return (
-      <Container>
+      <>
         <Header />
         <PageTitle>상세 상품 페이지</PageTitle>
         {this.state.wholeProducts.map((product, index) => {
@@ -172,7 +172,7 @@ class Product extends React.Component {
         })}
 
         <NavBar />
-      </Container>
+      </>
     )
   }
 }
@@ -182,15 +182,10 @@ const PageTitle = styled.div`
   font-size: 36px;
   font-weight: bold;
 `
-const Container = styled.div`
-  width: 768px;
-  margin: 0 auto;
-  background-color: white;
-`
 const ProductContainer = styled.div`
   width: 630px;
   margin: 0 auto;
-  margin-top: 272px;
+  margin-top: calc((100vh - 300px) / 4);
   padding: 52px 34px;
   border: 4px solid #2d3ff3;
   border-radius: 15px;
