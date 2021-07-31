79f606c704e47f5acd7a5999cd83a819658d9a3f
hami-dev <rinrin.xml@gmail.com>
2021-07-31 20:34:59 +0900

    style : constant 설정

--- src/constant.js
+++ src/constant.js
@@ -1,1 +1,7 @@
 //constant.js
+export const PRODUCT_CNT_LIMIT = 10
+
+export const ALERT_NO_MORE_RANDOM_PRODUCT =
+  "모든 상품이 '관심없음' 처리 되어 있습니다."
+export const ALERT_NO_MORE_RANDOM_PRODUCT_EXCEPT_CURRENT =
+  "해당 상품 외 모든 상품이 '관심 없는 상품'입니다."
